4486b24a568b3f43e333c37016eafa7701359fe8
KL Belgrad / Savic <Aleksandar.Savic@klingspor.de>
2022-03-10 15:58:53 +0100

    bugfix

--- src/Dashboard/Message.js
+++ src/Dashboard/Message.js
@@ -7,7 +7,7 @@ function Message (props) {
         <React.Fragment>
             <div className="message-container" >
                 <span className="message-header" >
-                    <img src={props.avatar} width="36px"/>
+                    <img src={props.avatar} width="36px" alt="mesage sender avatar" />
                     <h5>{props.name}</h5>
                 </span>
                 <p>{props.message}</p></div>
